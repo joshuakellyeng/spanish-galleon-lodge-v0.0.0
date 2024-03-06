ae8313fad8420fe167102b5dac42c8deaa99bfc3
Joshua Kelly <joshuakellyeng@gmail.com>
2024-03-06 19:35:41 +0100

    edited the modal styles to ensure responsiveness

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -48,3 +48,25 @@ for (const item of dropDowns) {
 	};
 	item.addEventListener('click', onClick);
 }
+
+// footer modal code
+// open modal by id
+function openModal(id) {
+	document.getElementById(id).classList.add('open');
+	document.body.classList.add('jw-modal-open');
+}
+
+// close currently open modal
+function closeModal() {
+	document.querySelector('.jw-modal.open').classList.remove('open');
+	document.body.classList.remove('jw-modal-open');
+}
+
+window.addEventListener('load', function () {
+	// close modals on background click
+	document.addEventListener('click', (event) => {
+		if (event.target.classList.contains('jw-modal')) {
+			closeModal();
+		}
+	});
+});
